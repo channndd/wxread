114ab6d8e6faca301f58a4c2797113a29ab39daa
Channn <86246526+channndd@users.noreply.github.com>
2024-12-30 14:46:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -9,7 +9,7 @@ github action部署或本地部署
 """
 
 # 阅读次数 默认120次/60分钟
-READ_NUM = int(os.getenv('READ_NUM', '120'))
+READ_NUM = int(os.getenv('READ_NUM', '480'))
 # pushplus or telegram
 PUSH_METHOD = "" or os.getenv('PUSH_METHOD')
 # push-plus
@@ -22,67 +22,88 @@ curl_str = os.getenv('WXREAD_CURL')
 
 # 对应替换
 headers = {
-    "accept": "application/json, text/plain, */*",
-    "accept-language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6,ko;q=0.5",
-    "baggage": "sentry-environment=production,sentry-release=dev-1727596539903,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=d3cc3a94f5244647b8064ecd77eb8ba6",
-    "cache-control": "no-cache",
-    "content-type": "application/json;charset=UTF-8",
-    "dnt": "1",
-    "origin": "https://weread.qq.com",
-    "pragma": "no-cache",
-    "priority": "u=1, i",
-    "referer": "https://weread.qq.com/web/reader/ce032b305a9bc1ce0b0dd2akd2d32c50249d2ddea18fb39",
-    "sec-ch-ua": "\"Microsoft Edge\";v=\"129\", \"Not=A?Brand\";v=\"8\", \"Chromium\";v=\"129\"",
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": "\"Windows\"",
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "same-origin",
-    "sentry-trace": "d3cc3a94f5244647b8064ecd77eb8ba6-93b39e13fa4e5fd6",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36 Edg/129.0.0.0"
+    'accept': 'application/json, text/plain, */*',
+    'accept-language': 'zh-CN,zh;q=0.9',
+    'baggage': 'sentry-environment=production,sentry-release=dev-1730698697208,sentry-public_key=ed67ed71f7804a038e898ba54bd66e44,sentry-trace_id=6d609675597e43e297e431269f0ffd99',
+    'content-type': 'application/json;charset=UTF-8',
+    # 'cookie': 'ptcz=03a14e17fb3e63c2f5f20910cfdfaf303bfbafef46bb38cf11e79506a54d1dc9; pgv_pvid=5915400066; fqm_pvqid=2b939038-5dea-4db4-b7b7-475a124e202f; o2_uin=945266540; qq_domain_video_guid_verify=d580433ca06286e4; _qimei_uuid42=18504122425100032708457a7e86f079cb9b43eff5; _qimei_fingerprint=17f16a2c4f47e35167705b07c1496dbf; _qimei_q36=; _qimei_h38=2db8d00d2708457a7e86f07902000008e18504; o_cookie=945266540; eas_sid=11p7111644B6J3H63782c1q9s1; LW_uid=U1X7q1D6V4A643t6Y9s0S0E3P0; RK=x/kJNQAzSA; LW_sid=z1M731y8y4O2j8j930H1u5s460; pac_uid=0_ieb50JsH1RGTK; suid=user_0_ieb50JsH1RGTK; wr_vid=274211715; wr_rt=web%40~3hk~Nsctu7j~~uDvDG_AL; wr_localvid=f8c326b0810582383f8cd1a; wr_name=200%20ok; wr_avatar=https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FPiajxSqBRaEIYWk8o7kuQ5heqG6WFVyMz98HeYQokX2Je6LvbtX1yb4fvqOKsO9bicQqx1Wicds7R2yTPdtug8fZu20qojZxCbGDWOwmhNbPpvuMQFE8tjE0w%2F132; wr_gender=1; wr_theme=white; wr_pf=NaN; wr_fp=1465892762; _clck=3918809406|1|frz|0; wr_gid=266269509; wr_skey=TpUgj3wn',
+    'origin': 'https://weread.qq.com',
+    'priority': 'u=1, i',
+    'referer': 'https://weread.qq.com/web/reader/0373243072163158037fbea',
+    'sec-ch-ua': '"Google Chrome";v="131", "Chromium";v="131", "Not_A Brand";v="24"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-origin',
+    'sentry-trace': '6d609675597e43e297e431269f0ffd99-9d9e728b3f961af6',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'
 }
 #
 # #对应替换
 cookies = {
-    "RK": "oxEY1bTnXf",
-    "ptcz": "53e3b35a9486dd63c4d06430b05aa169402117fc407dc5cc9329b41e59f62e2b",
-    "pac_uid": "0_e63870bcecc18",
-    "iip": "0",
-    "_qimei_uuid42": "183070d3135100ee797b08bc922054dc3062834291",
-    "_qimei_fingerprint": "28ec521da86d1fbc149479d2aa40f951",
-    "_qimei_q36": "",
-    "_qimei_h38": "cb6de4e4797b08bc922054dc02000005818307",
-    "pgv_pvid": "1212703189",
-    "fqm_pvqid": "50bb40ea-985c-4d11-9cea-7dfefe6ea1ca",
-    "_clck": "15sxecs|1|fl1|0",
-    "qq_domain_video_guid_verify": "004329d456c0ef18",
-    "wr_vid": "346607432",
-    "wr_localvid": "6a8327b0814a8cf486a8884",
-    "wr_name": "%E6%9C%AC%20%E6%97%A0%20%E9%81%93",
-    "wr_gender": "1",
-    "wr_rt": "web%40dz_AYa7CIYk07_ucDIb_AL",
-    "wr_avatar": "https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FMCpyjIyiaHicBXjh38REzNMA1xXiaeoWJ321CicmRcyMzeSibgDp1z6XC1FVr4szNr4PUsfIqEPRNXa4l9h2NGQsZDg%2F132",
-    "wr_fp": "1659424119",
-    "wr_pf": "NaN",
-    "wr_skey": "ivnZkd2_"
+    'ptcz': '03a14e17fb3e63c2f5f20910cfdfaf303bfbafef46bb38cf11e79506a54d1dc9',
+    'pgv_pvid': '5915400066',
+    'fqm_pvqid': '2b939038-5dea-4db4-b7b7-475a124e202f',
+    'o2_uin': '945266540',
+    'qq_domain_video_guid_verify': 'd580433ca06286e4',
+    '_qimei_uuid42': '18504122425100032708457a7e86f079cb9b43eff5',
+    '_qimei_fingerprint': '17f16a2c4f47e35167705b07c1496dbf',
+    '_qimei_q36': '',
+    '_qimei_h38': '2db8d00d2708457a7e86f07902000008e18504',
+    'o_cookie': '945266540',
+    'eas_sid': '11p7111644B6J3H63782c1q9s1',
+    'LW_uid': 'U1X7q1D6V4A643t6Y9s0S0E3P0',
+    'RK': 'x/kJNQAzSA',
+    'LW_sid': 'z1M731y8y4O2j8j930H1u5s460',
+    'pac_uid': '0_ieb50JsH1RGTK',
+    'suid': 'user_0_ieb50JsH1RGTK',
+    'wr_vid': '274211715',
+    'wr_rt': 'web%40~3hk~Nsctu7j~~uDvDG_AL',
+    'wr_localvid': 'f8c326b0810582383f8cd1a',
+    'wr_name': '200%20ok',
+    'wr_avatar': 'https%3A%2F%2Fthirdwx.qlogo.cn%2Fmmopen%2Fvi_32%2FPiajxSqBRaEIYWk8o7kuQ5heqG6WFVyMz98HeYQokX2Je6LvbtX1yb4fvqOKsO9bicQqx1Wicds7R2yTPdtug8fZu20qojZxCbGDWOwmhNbPpvuMQFE8tjE0w%2F132',
+    'wr_gender': '1',
+    'wr_theme': 'white',
+    'wr_pf': 'NaN',
+    'wr_fp': '1465892762',
+    '_clck': '3918809406|1|frz|0',
+    'wr_gid': '266269509',
+    'wr_skey': 'TpUgj3wn'
 }
 
 # 保留| 默认读三体，其它书籍自行测试时间是否增加
+# data = {
+#     "appId": "wb182564874663h152492176",
+#     "b": "ce032b305a9bc1ce0b0dd2a",
+#     "c": "7cb321502467cbbc409e62d",
+#     "ci": 70,
+#     "co": 0,
+#     "sm": "[插图]第三部广播纪元7年，程心艾AA说",
+#     "pr": 74,
+#     "rt": 30,
+#     "ts": 1727660516749,
+#     "rn": 31,
+#     "sg": "991118cc229871a5442993ecb08b5d2844d7f001dbad9a9bc7b2ecf73dc8db7e",
+#     "ct": 1727660516,
+#     "ps": "b1d32a307a4c3259g016b67",
+#     "pc": "080327b07a4c3259g018787",
+# }
 data = {
-    "appId": "wb182564874663h152492176",
-    "b": "ce032b305a9bc1ce0b0dd2a",
-    "c": "7cb321502467cbbc409e62d",
-    "ci": 70,
-    "co": 0,
-    "sm": "[插图]第三部广播纪元7年，程心艾AA说",
-    "pr": 74,
-    "rt": 30,
-    "ts": 1727660516749,
-    "rn": 31,
-    "sg": "991118cc229871a5442993ecb08b5d2844d7f001dbad9a9bc7b2ecf73dc8db7e",
-    "ct": 1727660516,
-    "ps": "b1d32a307a4c3259g016b67",
-    "pc": "080327b07a4c3259g018787",
+    'appId': 'wb182564874663h1256619504',
+    'b': '0373243072163158037fbea',
+    'c': '6f4322302126f4922f45dec',
+    'ci': 18,
+    'co': 371,
+    'sm': '为什么我是命运一我知道自己的命运。总有一',
+    'pr': 99,
+    'rt': 4,
+    'ts': 1735195858596,
+    'rn': 473,
+    'sg': '0f7999b48d644aa7d3972769a430e9d27d553e432bf31883be25419791542012',
+    'ct': 1735195858,
+    'ps': '7fa327507a57b2deg0118ec',
+    'pc': 'def325e07a57b2deg012ce2'
 }
 
 
